ecab51347a9870fb3a3a01a4f6a3cac8d0535c14
Abbosjon Karshiev <s284350@studenti.polito.it>
2025-12-22 14:01:47 +0100

    global handler mdilware has been added together with app.all which delegete error to global error handler the routes which doesnot exit

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,22 @@
 import  express  from "express";
 import { type Application, type Request,type Response } from "express";
 import router from "./routes/noteRouters.js"
-
+import AppError from "./models/AppError.js"
+import { globalErrorHandler } from "./middlewares/errorHandlerMiddleware.js";
 const app:Application = express();
 
 
 
 
 app.use(express.json())
-app.use((req, res, next) => {
-  console.log(`Incoming Request: ${req.method} ${req.url}`);
-  next();
-});
+
 
 // Register routes
 app.use("/notes",router)
 
 //Global error handler
-app.use((err:Error, req:Request, res:Response,next:Function)=>{
-res.status(500).json({message:err.message});
 
-});
+
+app.use(globalErrorHandler);
 
 export default app;
--- src/middlewares/errorHandlerMiddleware.ts
+++ src/middlewares/errorHandlerMiddleware.ts
@@ -1,10 +1,22 @@
 import type { Request, Response, NextFunction } from 'express';
+import AppError from '../models/AppError.js';
 
-export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.error(err.stack);
+export const globalErrorHandler=(err:AppError|Error,req:Request,res:Response,next:NextFunction)=>{
 
-  res.status(500).json({
-    message: err.message,
-    ...(process.env.NODE_ENV === 'development' && { stack: err.stack })
-  });
-};
+if(err instanceof AppError){
+  err.status=err.status||'error'
+  err.statusCode=err.statusCode||500
+
+  res.status(err.statusCode).json({
+    status:err.status,
+    message:err.message,
+
+  })
+}
+
+res.status(500).json({
+  status:'error',
+  message:'Bad Request'
+})
+
+}
